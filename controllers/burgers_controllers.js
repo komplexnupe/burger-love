ae7a4b1c73d7f262f52d5b5b8969a787e122444e
Komplexnupe <leonard.love87@icloud.com>
2020-09-11 00:23:50 -0400

    udpate post in burger_controller & path on main

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -16,7 +16,7 @@ router.get("/", function(req, res) {
   });
 
   router.post("/api/burgers", function(req, res) {
-    cat.create([
+    burger.create([
       "burger_name", "devoured"
     ], [
       req.body.burger_name, req.body.devoured
